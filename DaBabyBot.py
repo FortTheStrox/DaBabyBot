a753f6ea1f4150b94a43765111ca9d4dc1f6896e
Jared Erlien <JaredErl@protonmail.ch>
2021-04-22 19:47:42 -0500

    Init commit

---
+++ DaBabyBot.py
@@ -0,0 +1,14 @@
+# DaBabyBot.py
+'''
+@os for operating system interaction
+@random for random selection functions
+@discord for basic discord interaction
+@dotenv for env functions
+@urllib.parse, @urllib.request, @re
+'''
+import os
+import random
+import discord
+import urllib.parse, urllib.request, re
+from discord.ext import commands
+from dotenv import load_dotenv
