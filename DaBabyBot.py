5838002247c8abbbafafb650a5793879bdcd3ae8
Andrew Castillo <andrewcast32@gmail.com>
2021-04-23 17:55:05 -0700

    Dababy joins on command

--- DaBabyBot.py
+++ DaBabyBot.py
@@ -44,6 +44,28 @@ async def on_message(message):
     print(f'A user sent a message')
     await client.process_commands(message)
 
+# need this for voice package: pip install -U discord.py[voice]
+# Trying to get the bot to join vc
+@client.command(pass_context = True)
+async def join(ctx):
+    # if the user is in a vc it'll run the command but if they're not, then it'll send a message
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else: 
+        await ctx.send("Bruh moment, you aren't in a fucking vc")
+
+
+# Getting the bot to leave vc
+@client.command(pass_context = True)
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send('Dababy gotta go')
+    else:
+        await ctx.send('Boy if you dont shut the hell up')
+
+
 # Shutdowns the bot and will only shut down if call is from owner of bot
 @client.command()
 @commands.is_owner()
