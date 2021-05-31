5fa78abf4c25c65c0815e28b0e3cf8533e24f480
Jared Erlien <JaredErl@protonmail.ch>
2021-05-31 14:52:58 -0500

    fixed shutdown and leave command, added voice dababy clips as well as homegrown audio

--- DaBabyBot.py
+++ DaBabyBot.py
@@ -2,18 +2,18 @@
 '''
 @re for regex patterns
 @os for operating system interaction
-@random for random selection functions
+@time for sleep
 @discord for basic discord interaction
 @dotenv for env functions
-@urllib.parse, @urllib.request, @re
+@random for choosing dababy audio
 '''
 import re
 import os
-import random
+import time
 import discord
-import urllib.parse, urllib.request, re
 from discord.ext import commands
 from dotenv import load_dotenv
+import random
 
 # load onv and set token from .env file within same directory
 load_dotenv()
@@ -68,32 +68,44 @@ async def on_message(message):
     print(f'A user sent a message')
     await client.process_commands(message)
 
-# need this for voice package: pip install -U discord.py[voice]
-# Trying to get the bot to join vc
 @client.command(pass_context = True)
-async def dababy(ctx):
-    # if the user is in a vc it'll run the command but if they're not, then it'll send a message
-    if (ctx.author.voice):
-        channel = ctx.message.author.voice.channel
-        await channel.connect()
-    else: 
-        await ctx.send("Bruh moment, you aren't in a fucking vc")
-
+async def idolVoice(ctx):
+    
+    # Gets voice channel of message author
+    voice_channel = ctx.author.voice.channel
+    channel = None
+    if voice_channel != None:
+        channel = voice_channel.name
+        vc = await voice_channel.connect()
+        vc.play(discord.FFmpegPCMAudio(source=f"audio/dababy{random.randint(1,5)}.m4a", executable="A:/Downloads/ffmpeg-N-102631-gbaf5cc5b7a-win64-gpl/bin/ffmpeg.exe"))
+        # Sleep while audio is playing.
+        while vc.is_playing():
+            time.sleep(.1)
+        await vc.disconnect()
+    else:
+        await ctx.send(str(ctx.author.name) + "is not in a channel.")
+    # Delete command after the audio is done playing.
+    await ctx.message.delete()
 
 # Getting the bot to leave vc
 @client.command(pass_context = True)
 async def leave(ctx):
-    if (ctx.voice_client):
-        await ctx.guild.voice_client.disconnect()
-        await ctx.send('Dababy gotta go')
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if not voice is None:
+        if voice.is_connected():
+            await ctx.send('Dababy gotta go')
+            await voice.disconnect()
+        else:
+            # no channel to leave
+            await ctx.send('Boy if you dont shut the hell up')
     else:
+        # no channel to leave
         await ctx.send('Boy if you dont shut the hell up')
 
 
 # Shutdowns the bot and will only shut down if call is from owner of bot
 @client.command()
-@commands.is_owner()
 async def shutdown(ctx):
-    await ctx.bot.logout()
+    await client.close()
 
 client.run(token)
